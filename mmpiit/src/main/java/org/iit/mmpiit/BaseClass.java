45a37560dccee34093339b1df7f786bd065ce6fe
sudheer51 <sudheer694@gmail.com>
2019-05-25 06:40:28 +0530

    Update BaseClass.java

--- mmpiit/src/main/java/org/iit/mmpiit/BaseClass.java
+++ mmpiit/src/main/java/org/iit/mmpiit/BaseClass.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+//Adding a comment
 public class BaseClass {
 	WebDriver driver;
 	public void invokeBrowser()
